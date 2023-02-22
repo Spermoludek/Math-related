b641fc24a4d08638e6810c45013645afed803fbe
Doctor of sex and physics <98488700+Spermoludek@users.noreply.github.com>
2023-02-22 19:31:38 +0100

    Add files via upload

---
+++ ArmstrongNumber.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int n, sum, num = 0;
+int getPower(int num) {
+    if (num == 0) {
+        return n; 	// Recursion termination, we need to get the number's length
+    }
+    n++;
+    return getPower(num / 10);
+}
+int calculateSum(int num, int n) {
+        if (num == 0) {
+        return sum; // Recursion termination 
+    }
+    sum += pow((num % 10), n); 		// We are summing up each digit raised to the length's power
+    return calculateSum(num / 10, n);
+}
+
+
+int main() {
+    cout << "Please enter a number: ";
+    cin >> num;
+    if (calculateSum(num, getPower(num)) == num) {
+        cout << "Number you entered is an Armstrong number";
+    }
+    else {
+        cout << "Number you entered is not an Armstrong number";
+    }
+    return 0;
+}
+/*
+ABSTRACT:
+An Armstrong number is a number that's equal to sum of its digits raised to the power of number's length
+
+For instance 153 = 1^3 + 5^3 + 3^3
+
+Here are some Armstrong numbers:
+153, 370, 407, 8208, 1634, 9474. 548834
+
+
+Please note that this is a terrible way of doing this and there probably is an algorithmically faster method, but we all start somewhere
+*/
